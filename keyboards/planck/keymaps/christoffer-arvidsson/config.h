7af35927d8c692fff2328fa6dbd1bc523ffe6af6
Christoffer Arvidsson <christoffer@arvidson.nu>
2023-10-20 23:26:03 +0200

    Add back quick tap term
    Seems to have been the default before to not repeat a key on
    tap hold tap in quick succession, but now it's default to repeat it.
    This makes it not repeat it.

--- keyboards/planck/keymaps/christoffer-arvidsson/config.h
+++ keyboards/planck/keymaps/christoffer-arvidsson/config.h
@@ -24,8 +24,7 @@
 #undef TAPPING_TERM
 #define TAPPING_TERM 160
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+#define QUICK_TAP_TERM 0
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
